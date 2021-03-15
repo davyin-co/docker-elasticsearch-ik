268f6ef956c69ced9a3b087e6865244b817e4014
terry zhang <zterry@qq.com>
2021-03-15 12:35:10 +0800

    update version

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ sedStr="
 sed -e "$sedStr" $1
 }
 
-versions=(7.3.2 7.4.2 7.5.2 7.6.2 7.7.1 7.8.1 7.9.3 7.10.2 7.11.2)
+versions=(7.3.2 7.4.2 7.5.2 7.6.2 7.7.1 7.8.1 7.9.3 7.10.1 7.11.1)
 for version in ${versions[*]}; do
   if [ ! -d $version ]; then
     mkdir $version
