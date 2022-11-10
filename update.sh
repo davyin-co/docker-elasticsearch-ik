9bd01c9fd83c0b127f2fa477c3226f13c1c3757d
Terry Zhang <zterry@qq.com>
2022-11-10 09:59:46 +0000

    compitable with ik version

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ sedStr="
 sed -e "$sedStr" $1
 }
 
-versions=(7.10.1 7.11.2 7.12.1 7.13.4 7.14.2 7.16.3 7.17.7 8.1.3 8.2.3 8.3.3 8.4.3 8.5.0)
+versions=(7.10.1 7.11.2 7.12.1 7.13.4 7.14.2 7.16.3 7.17.6 8.1.3 8.2.3 8.3.3 8.4.3)
 for version in ${versions[*]}; do
   if [ ! -d $version ]; then
     mkdir $version
